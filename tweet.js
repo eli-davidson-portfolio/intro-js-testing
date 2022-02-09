626708201ff40e610b6612f72492209b6464f1e2
Eli Davidson <eli@shadowlab.dev>
2022-02-09 15:25:37 -0700

    Create tweet class

--- tweet.js
+++ tweet.js
@@ -1,4 +1,27 @@
 class Tweet {
+    constructor (sender, content, link) {
+        this.sender = sender;
+        this.content = content;
+        this.link = `no link provided`;
+        this.likes = 0;
+        this.retweets = 0;
+        this.replyCount = 0;
+        this.replies = [];
+        if (link) {
+            this.link = link;
+        }
+    }
+    like () {
+        this.likes++;
+    }
+    retweet() {
+        this.retweets++;
+    }
+    reply(replyString) {
+        this.replies.push(replyString)
+        this.replyCount++;
+    }
+
 
 }
 
